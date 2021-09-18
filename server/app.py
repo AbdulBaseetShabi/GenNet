00bafa962fd832f44256a07ca1bf0beba68a857a
cg <31290848+slightlyskepticalpotat@users.noreply.github.com>
2021-09-18 14:14:00 -0400

    add api functions

--- server/app.py
+++ server/app.py
@@ -4,7 +4,6 @@ from helpers import *
 app = Flask(__name__)
 
 @app.route("/")
-@admin_access
 def warning():
     return "This is the API URL address. Use GenNet at [website]."
 
@@ -14,4 +13,46 @@ def login():
 
 @app.route("/register", methods=["POST"])
 def register():
+    pass
+
+@app.route("/admin/adduser", methods=["POST"])
+@admin_access
+def add_user():
+    pass
+
+@app.route("/admin/removeuser", methods=["POST"])
+@admin_access
+def remove_user():
+    pass
+
+@app.route("/admin/editalbum", methods=["POST"])
+@admin_access
+def edit_album():
+    pass
+
+@app.route("/user/leavetree", methods=["POST"])
+@login_access
+def leave_tree():
+    pass
+
+@app.route("/user/viewperms", methods=["GET"])
+@login_access
+def view_perms():
+    pass
+
+@app.route("/user/viewalbum", methods=["GET"])
+@login_access
+def view_album():
+    pass
+
+@app.route("/user/deleteaccount", methods=["POST"])
+@login_access
+def delete_account():
+    # auth them again here
+    pass
+
+@app.route("/user/editaccount", methods=["POST"])
+@login_access
+def edit_account():
+    # for changing settings/data
     pass
