bbf26e18c04c5ef9a4dfae3a3dafb6c274d2534a
cg <31290848+slightlyskepticalpotat@users.noreply.github.com>
2021-09-18 19:26:57 -0400

    outline params

--- server/app.py
+++ server/app.py
@@ -22,47 +22,52 @@ print(conn.closed)
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def warning():
     return "This is the API URL address. Use GenNet at [website]."
 
 @app.route("/login", methods=["POST"])
 def login():
-    pass
+    email = request.args.get("email")
+    password = request.args.get("password")
 
 @app.route("/register", methods=["POST"])
 def register():
-    pass
+    email = request.args.get("email")
+    password = request.args.get("password")
+    password_conf = request.args.get("password_conf")
 
 @app.route("/admin/adduser", methods=["POST"])
 @admin_access
 def add_user():
-    pass
+    email = request.args.get("email")
+    password = request.args.get("password")
 
 @app.route("/admin/removeuser", methods=["POST"])
 @admin_access
 def remove_user():
-    pass
+    email = request.args.get("email")
+    delete_trees = request.args.get("delete_trees")
 
 @app.route("/admin/editalbum", methods=["POST"])
 @admin_access
 def edit_album():
-    pass
+    family = request.args.get("family")
 
 @app.route("/user/leavetree", methods=["POST"])
 @login_access
 def leave_tree():
-    pass
+    family = request.args.get("family")
 
 @app.route("/user/viewperms", methods=["GET", "POST"])
 @login_access
 def view_perms():
-    pass
+    family = request.args.get("family")
 
 @app.route("/user/viewalbum", methods=["GET", "POST"])
 @login_access
 def view_album():
-    pass
+    family = request.args.get("family")
 
 @app.route("/user/deleteaccount", methods=["POST"])
 @login_access
@@ -74,4 +79,6 @@ def delete_account():
 @login_access
 def edit_account():
     # for changing settings/data
-    pass
+    # all-purpose function for accounts
+    key = request.args.get("key")
+    value = request.args.get("value")
