b8cd8946e53308057dff8070e36782d511fee811
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-09-18 17:24:58 -0400

    project route structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
+
+//routes
+import LoginPage from "./routes/login/login"
+import HomePage from "./routes/home/home"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  
+
   render() {
-    return <Router></Router>;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/signup"></Route>
+          <Route path="/login">
+            <LoginPage/>
+          </Route>
+          <Route path="/home">
+            <HomePage/>
+          </Route>
+          <Route path="/">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </Router>
+    );
   }
 }
 
