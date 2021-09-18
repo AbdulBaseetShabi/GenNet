5484c7de8cfd2cafe7c7166b947e2a7126fa9edf
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-09-18 13:44:26 -0400

    React Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  
+  render() {
+    return <Router></Router>;
+  }
+}
+
+export default App;
