468b80221075069cb4bfbacbb576311b5445bfaa
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-09-19 00:46:29 -0400

    codee refactor

--- client/src/widget/navigationBar/navigationBar.js
+++ client/src/widget/navigationBar/navigationBar.js
@@ -1,31 +1,73 @@
 import React from "react";
 import "./navigationBar.css";
-  
+
 const navItem = {
   paddingLeft: "2%",
   cursor: "pointer",
-}
+};
 
 function NavigationBar(props) {
   return (
     <div id="navigation-bar-container">
-      <label className="full-width text-center" style={{marginBottom: "0rem"}}>Navigation</label>
-      <hr className="seperator" style={{margin: "0.5rem auto", borderTop: "1px solid #fffffe"}} />
+      <label
+        className="full-width text-center"
+        style={{ marginBottom: "0rem" }}
+      >
+        Navigation
+      </label>
+      <hr
+        className="seperator"
+        style={{ margin: "0.5rem auto", borderTop: "1px solid #fffffe" }}
+      />
       <div className="navigation-item-container">
-        <div className="navigation-item" onClick={(e)=>props.changePath(0)}>
-          <i className="fas fa-home"></i>
+        <div
+          className="navigation-item row"
+          onClick={(e) => props.changePath(0)}
+        >
+          <div style={{ width: "10%", textAlign: "center" }}>
+            <i className="fas fa-home"></i>
+          </div>
           <label style={navItem}>Home</label>
         </div>
-        <div className="navigation-item" onClick={(e)=>props.changePath(1)}>
-          <i className="fas fa-bell"></i>
+        <div
+          className="navigation-item row"
+          onClick={(e) => props.changePath(2)}
+        >
+          <div style={{ width: "10%", textAlign: "center" }}>
+            <i className="fas fa-clipboard"></i>
+          </div>
+          <label style={navItem}>Journals</label>
+        </div>
+        <div
+          className="navigation-item row"
+          onClick={(e) => props.changePath(4)}
+        >
+          <div style={{ width: "10%", textAlign: "center" }}>
+            <i className="fas fa-bell"></i>
+          </div>
           <label style={navItem}>Notifications</label>
         </div>
-        <div className="navigation-item" onClick={(e)=>props.changePath(2)}>
-          <i className="fas fa-cog"></i>
-          <label style={navItem}>Settings</label>
+        <div
+          className="navigation-item row"
+          onClick={(e) => props.changePath(4)}
+        >
+          <div style={{ width: "10%", textAlign: "center" }}>
+            <i className="fas fa-cog"></i>
+          </div>
+          <label style={navItem}>Account</label>
         </div>
       </div>
-      <hr className="seperator" style={{margin: "0.5rem auto", borderTop: "1px solid #fffffe"}} />
+      {/* <hr
+        className="seperator"
+        style={{ margin: "0.5rem auto", borderTop: "1px solid #fffffe" }}
+      /> */}
+
+      <div className="text-center btn" id="log-out-button" onClick={(e) => {props.logOut()}}>
+        <label style={navItem}>
+          Log Out
+          <i class="fas fa-sign-out-alt" style={{paddingLeft: "2px"}}></i>
+        </label>
+      </div>
     </div>
   );
 }
