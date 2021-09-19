16016af933f9d4513744bf8ca3de677e4b921ba9
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-09-19 02:57:36 -0400

    adding a new member to a family tree

--- client/src/routes/familyTree/familyTree.js
+++ client/src/routes/familyTree/familyTree.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Member from "../../widget/member/member";
+import AddMember from "../addModals/add-member";
 import "./familyTree.css";
 
 const inputContainer = { width: "80%", margin: "0 auto" };
@@ -7,8 +8,14 @@ const inputContainer = { width: "80%", margin: "0 auto" };
 class FamilyTree extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showModal: false,
+    };
+    this.addMember = this.addMember.bind(this);
+    this.openCloseModal = this.openCloseModal.bind(this);
+    this.memberToAdd = this.memberToAdd.bind(this);
     this.search_value = "";
+    this.new_member = "";
   }
 
   changeSearchValue(value) {
@@ -22,10 +29,40 @@ class FamilyTree extends React.Component {
     }
   }
 
+  memberToAdd(value){
+    this.new_member = value;
+  }
+
+  addMember() {
+    let is_successful_call = true;
+    window.alert(this.new_member);
+    if (is_successful_call) {
+      
+      this.openCloseModal();
+    }
+  }
+
+  openCloseModal() {
+    this.setState((prevState, prevProps) => {
+      if (prevState.showModal) {
+        this.new_member = "";
+        return { showModal: false };
+      } else {
+        return { showModal: true };
+      }
+    });
+  }
+
   render() {
     let family_name = "default";
     return (
       <div id="family-tree-container" className="page">
+        <AddMember
+          showModal={this.state.showModal}
+          addMember={this.addMember}
+          closeModal={this.openCloseModal}
+          memberToAdd = {this.memberToAdd}
+        />
         <label className="text-center full-width">
           Welcome to the <i>{family_name.toUpperCase()}</i> family
         </label>
@@ -50,11 +87,17 @@ class FamilyTree extends React.Component {
             </div>
           </div>
         </div>
-        <button id="signup-button" className="full-width btn custom-button">
+        <button
+          id="signup-button"
+          className="full-width btn custom-button"
+          onClick={(e) => {
+            this.openCloseModal();
+          }}
+        >
           Add New Member
         </button>
         <div id="family-tree-search-result">
-          <Member viewJournal={this.props.viewJournal}/>
+          <Member viewJournal={this.props.viewJournal} />
         </div>
       </div>
     );
